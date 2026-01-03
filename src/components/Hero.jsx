3973612dce7e0119c14c7feba68b17bdb917467f
Ramzan <muhammadramzan3043@gmail.com>
2026-01-03 14:33:13 +0500

    ramzan

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,6 +16,7 @@ export default function Hero({ isFullWidth }) {
                 className="object-cover object-center opacity-25 w-full h-175"
             />
             <div className="absolute top-0 p-8 md:p-16 lg:p-24 text-center w-full h-full flex flex-col  items-center">
+                <h3 className="text-[#FF9203] font-semibold uppercase ">{data.pageBy.heroSection?.eyebrowText}</h3>
                 <h1 className="text-4xl font-bold mb-4">{data.pageBy.heroSection?.primaryHeading}</h1>
                 <p className="text-lg">{data.pageBy.heroSection?.description}</p>
             </div>
--- src/graphql/logo.js
+++ src/graphql/logo.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const GET_SITE_LOGO = gql`
   query GetLogo {
-    mediaItem(id: 27, idType: DATABASE_ID) {
+    mediaItem(id: 55, idType: DATABASE_ID) {
       sourceUrl
       altText
     }
