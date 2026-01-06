594a843db43d43cdc7adcc84e56c480ce0942bdf
Ramzan <muhammadramzan3043@gmail.com>
2026-01-06 17:06:56 +0500

    ramzan

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,10 +15,13 @@ export default function Hero({ isFullWidth }) {
                 alt=""
                 className="object-cover object-center opacity-25 w-full h-175"
             />
-            <div className="absolute top-0 p-8 md:p-16 lg:p-24 text-center w-full h-full flex flex-col  items-center">
-                <h3 className="text-[#FF9203] font-semibold uppercase ">{data.pageBy.heroSection?.eyebrowText}</h3>
-                <h1 className="text-4xl font-bold mb-4">{data.pageBy.heroSection?.primaryHeading}</h1>
-                <p className="text-lg">{data.pageBy.heroSection?.description}</p>
+            <div className="absolute top-0 p-8 md:p-16 lg:p-24 text-center w-full h-full flex flex-col items-end justify-start">
+                <div className="flex items-start flex-col">
+                    <h3 className="text-(--primary) font-semibold uppercase text-[15px]">{data.pageBy.heroSection?.eyebrowText}</h3>
+                    <h1 className="font-bold text-[50px] leading-15">{data.pageBy.heroSection?.primaryHeading}</h1>
+                    <h1 className="font-bold text-(--secondary) italic text-[70px] leading-19">{data.pageBy.heroSection?.highlightedHeading}</h1>
+                    <p className="text-lg w-200 text-left">{data.pageBy.heroSection?.description}</p>
+                </div>
             </div>
         </div>
     );
