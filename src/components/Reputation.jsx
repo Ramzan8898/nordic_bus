8c6a6980c40f30faeaeb99c99be658c6b96c7ad6
Ramzan <muhammadramzan3043@gmail.com>
2026-01-24 11:52:18 +0500

    ramzan

--- src/components/Reputation.jsx
+++ src/components/Reputation.jsx
@@ -1,35 +1,38 @@
 import { Link } from "react-router-dom";
 import CountUp from 'react-countup';
+import { use } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_REPUTATION_SECTION } from "../graphql/Reputation";
 export default function Reputation() {
+    const { data } = useQuery(GET_REPUTATION_SECTION);
     return (
         <div className="py-25 bg-[#0a0a0a]">
             <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-start">
                 <div className="w-full h-auto rounded-lg overflow-hidden">
                     {/* <video src="./src/assets/Reputation_video.mp4" autoPlay playsInline muted loop className="h-100 w-full" /> */}
-                    {/* <img src="./src/assets/Reputation.jpg" alt="" className="w-full h-150 object-cover object-bottom rounded-lg" /> */}
-                    <img src="./src/assets/Reputation.jpg" alt="" className="w-full h-150 object-cover object-bottom rounded-lg" />
+                    <img src={data?.pageBy?.reputation.leftImage.node.sourceUrl} alt="" className="w-full h-150 object-cover object-bottom rounded-lg" />
                 </div>
                 <div className="flex flex-col gap-2 mt-3">
-                    <h3 className="text-[#888] uppercase">Driven By Numbers</h3>
-                    <h1 className="text-(--primary) text-4xl">Data Driven Performance & Innovation</h1>
-                    <p className="text-white text-[18px] font-extralight">At Nordic Fleet, our reputation isn't just built on passion; it’s driven by data. From lap times to performance metrics, we harness cutting-edge analytics to ensure every race, every car, and every team is operating at peak performance.</p>
-                    <p className="text-white text-[18px] font-extralight">we pride ourselves on delivering top-tier performance, luxury, and precision. With a rich history of thrilling motorsport achievements and a passion for speed, we have earned the trust and respect of car enthusiasts and racing fans around the world.</p>
-                    <p className="text-white text-[18px] font-extralight">Our legacy speaks for itself, but the numbers tell the true story. With years of data-backed performance, we continue to push the limits of motorsport innovation. Join us as we race into the future, where data and reputation collide to redefine what’s possible on the track.</p>
+                    <h3 className="text-[#888] uppercase">{data?.pageBy?.reputation.eyebrow}</h3>
+                    <h1 className="text-(--primary) text-4xl">{data?.pageBy?.reputation.heading}</h1>
+                    <p className="text-white text-[18px] font-extralight">{data?.pageBy?.reputation.para1}</p>
+                    <p className="text-white text-[18px] font-extralight">{data?.pageBy?.reputation.para2}</p>
+                    <p className="text-white text-[18px] font-extralight">{data?.pageBy?.reputation.para3}</p>
                     <div className="self-start mt-2">
-                        <Link to="#" className="uppercase text-(--primary) text-lg px-12 py-3 border-2 border-(--primary) inline-block rounded-full ">Explore Our Fleet</Link>
+                        <Link to={data?.pageBy?.reputation.btnLink.url} className="uppercase text-(--primary) text-lg px-12 py-3 border-2 border-(--primary) inline-block rounded-full ">{data?.pageBy?.reputation.btnLink.title}</Link>
                     </div>
                     <div className="grid md:grid-cols-3 mt-10 gap-1" >
                         <div className="flex flex-col items-center">
-                            <CountUp start={0} end={100} className="text-white text-5xl font-extrabold" suffix="%" duration={3} />
-                            <p className="text-muted-foreground mb-2 text-2xl">Accident-Free Rentals</p>
+                            <CountUp start={0} end={data?.pageBy?.reputation?.counter?.[0]?.counterValue || 0} className="text-white text-5xl font-extrabold" suffix="%" duration={3} />
+                            <p className="text-muted-foreground mb-2 text-2xl">{data?.pageBy?.reputation?.counter?.[0]?.counterText || "Counter"}</p>
                         </div>
                         <div className="flex flex-col items-center">
-                            <CountUp start={0} end={34} className="text-white text-5xl font-extrabold" suffix="k+" duration={3} />
-                            <p className="text-muted-foreground mb-2 text-2xl">Happy Clients</p>
+                            <CountUp start={0} end={data?.pageBy?.reputation?.counter?.[1]?.counterValue || 0} className="text-white text-5xl font-extrabold" suffix="k+" duration={3} />
+                            <p className="text-muted-foreground mb-2 text-2xl">{data?.pageBy?.reputation?.counter?.[1]?.counterText || "Counter"}</p>
                         </div>
                         <div className="flex flex-col items-center">
-                            <CountUp start={0} end={210} className="text-white text-5xl font-extrabold" suffix="+" duration={3} />
-                            <p className="text-muted-foreground mb-2 text-2xl">Luxurious Cars</p>
+                            <CountUp start={0} end={data?.pageBy?.reputation?.counter?.[2]?.counterValue || 0} className="text-white text-5xl font-extrabold" suffix="+" duration={3} />
+                            <p className="text-muted-foreground mb-2 text-2xl">{data?.pageBy?.reputation?.counter?.[2]?.counterText || "Counter"}</p>
                         </div>
                     </div>
                 </div>
--- src/graphql/Reputation.js
+++ src/graphql/Reputation.js
@@ -4,12 +4,28 @@ export const GET_REPUTATION_SECTION = gql`
 query GetReputationSection {
     pageBy(uri: "/home"){
         reputation{
+        counter{
+                counterText
+                counterValue
+            }
             leftImage{
                 node{
                     sourceUrl
                     altText
                 }
             }
+            eyebrow
+            heading
+            para1
+            para2
+            para3
+            btnLink{
+                ... on AcfLink{
+                    url
+                    title
+                }
+            }
+            
         }
     }
 }
