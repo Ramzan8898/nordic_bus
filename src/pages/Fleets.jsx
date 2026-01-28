6b941b5bc18a763b0f286dacdb1d4929daca11f8
Ramzan <muhammadramzan3043@gmail.com>
2026-01-28 10:32:57 +0500

    ramzan

--- src/pages/Fleets.jsx
+++ src/pages/Fleets.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-export default function Fleets(){
+import Fleet from "../components/Fleet";
+
+export default function Fleets() {
     return (
-        <div className="text-3xl font-bold underline">This is Fleets Page</div>
-    )
+        <div className="bg-black text-white min-h-screen px-10 py-40">
+            <div className="container mx-auto">
+                <Fleet showViewAll={false} showLoadMore={true} />
+            </div>
+        </div>
+    );
 }
